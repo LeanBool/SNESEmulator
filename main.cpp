915e8e98280ef6c214b8ebefa46d376070a459c3
LeanBool <alexgraf44@gmail.com>
2021-04-10 16:32:30 +0200

    Added some tests

--- main.cpp
+++ main.cpp
@@ -119,6 +119,30 @@ bool testABI(CPU& cpu) {
 	return 1;
 }
 
+bool testABL(CPU& cpu) {
+	bool ret = false;
+	printf("\nABL Test:\n");
+
+	std::vector<int> inst = { 0xAF, 0xFE, 0x00, 0x02, 0xEA };
+	for (int i = 0; i < inst.size(); i++) {
+		cpu.write(i, inst[i]);
+	}
+	cpu.write(0x0200FE, 0xFF);
+	cpu.write(0x0200FF, 0x10);
+
+	do {
+		cpu.clock();
+	} while (cpu.opcode != 0xEA || cpu.cycles != 0);
+
+	ret = cpu.A == 0x10FF;
+
+	cpu.clearMemory();
+	cpu.clearRegisters();
+
+	return ret;
+}
+
+
 bool testIMM(CPU& cpu) {
 	bool ret = false;
 	printf("\nIMM Test:\n");
@@ -148,6 +172,7 @@ int main() {
 	bool aby = testABY(cpu);
 	bool aii = testAII(cpu);
 	bool abi = testABI(cpu);
+	bool abl = testABL(cpu);
 	bool imm = testIMM(cpu);
 
 	printf("\nAbsolute addressing test: %d\n", abs);
@@ -155,6 +180,7 @@ int main() {
 	printf("Absolute Y addressing test: %d\n", aby);
 	printf("Absolute indexed indirect addressing test: %d\n", aii);
 	printf("Absolute indirect adressing test: %d\n", abi);
+	printf("Absolute long addressing test: %d\n", abl);
 	printf("Immmediate addressing test: %d\n", imm);
 
 	return 0;
